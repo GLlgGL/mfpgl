277efc6bfd26d9f1f64fb79487a75182036839a4
GSTAR <44748406+GLlgGL@users.noreply.github.com>
2025-12-10 18:09:10 +0100

    Update vk.py

--- mediaflow_proxy/extractors/vk.py
+++ mediaflow_proxy/extractors/vk.py
@@ -15,7 +15,7 @@ UA = (
 class VKExtractor(BaseExtractor):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.mediaflow_endpoint = "stream"
+        self.mediaflow_endpoint = "hls_segment_proxy"
 
     async def extract(self, url: str, **kwargs) -> Dict[str, Any]:
         embed_url = self._normalize(url)
